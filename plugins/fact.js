35c01c2c7956febb2e035c7a3fcdde977ba48f3e
Hammad Tanoli <hamadzainab786@gmail.com>
2025-01-21 19:34:21 +0500

    Update fact.js

--- plugins/fact.js
+++ plugins/fact.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const fact = response.data.text;
 
         const funFact = `
-👾 *SILENT-SOBX-MD RANDOM FACT* 👾
+👾 *TANOLI-MD RANDOM FACT* 👾
 
 ${fact}
 
@@ -45,7 +45,7 @@ async (conn, mek, m, { from, q, reply }) => {
 😂 *Here's a random joke for you!* 😂
 *${joke.setup}*
 ${joke.punchline} 😄
-> *POWERED BY SOBIA MD*
+> *POWERED BY TANOLI MD*
 `;
         return reply(jokeMessage);
     } catch (e) {
