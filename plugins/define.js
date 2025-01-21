8e12af90049fa445c34908d0f9fe5f815836b1db
Hammad Tanoli <hamadzainab786@gmail.com>
2025-01-21 19:30:57 +0500

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -28,7 +28,7 @@ const wordInfo = `
 📝 *Example*: ${example}
 🔗 *Synonyms*: ${synonyms}
 
-*MADE WITH ♥ SOBIA MD*`;
+*MADE WITH ♥ TANOLI MD*`;
 
                                  return reply(wordInfo);
                              } catch (e) {
