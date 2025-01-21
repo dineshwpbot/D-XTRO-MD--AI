67bf5415f0c18bb71e843f21d57c185d76577c9a
Hammad Tanoli <hamadzainab786@gmail.com>
2025-01-21 19:39:53 +0500

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*SOBIA MD BOT PLUGINS*🏝️`
+> 🌈*TANOLI MD BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
